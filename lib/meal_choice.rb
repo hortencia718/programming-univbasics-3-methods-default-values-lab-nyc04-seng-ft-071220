6650b958ffa47074d5c0dff4652eba37c7f9e2a1
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-16 19:51:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice (broccoli,carrots,protein = 'tofu')
+def meal_choice (veg1,veg2,protein = 'tofu')
   puts "What a nutritious meal!"
 end
 # meal_choice(broccoli,carrots, protein = 'tofu')
