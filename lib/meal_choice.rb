8d757f629732b963e8fd7126f0a2b3534c748cc2
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-16 20:18:16 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,11 +3,8 @@ def meal_choice (veg1,veg2,protein = 'tofu')
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   meal_choice
 end
-# meal_choice(broccoli,carrots, protein = 'tofu')
 
-# def meal_choice(broccoli,carrots, protein = tofu)
-#   puts 
-# end
+
 
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
