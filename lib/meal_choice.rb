3f026628f31ccc7886cda44a8137bf6a053cbac6
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-16 19:30:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,11 @@
+def meal_choice (broccoli,carrots, tofu)
+  puts "What a nutritious meal!"
+end
+ meal_choice(broccoli,carrots,tofu)
+
+# def meal_choice(broccoli,carrots, protein = tofu)
+#   puts 
+# end
+
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
