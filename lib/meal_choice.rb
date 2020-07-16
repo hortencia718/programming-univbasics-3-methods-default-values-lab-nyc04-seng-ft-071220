56b5250e735b46522284ba37228b1533a29bc911
mirthful-generator-6192 <hortenciaecisneros@gmail.com>
2020-07-16 20:22:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 def meal_choice (veg1,veg2,protein = 'tofu')
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  # must return the last puts inorder to get it all r/t
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
